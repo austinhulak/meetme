cdcbb8ca7eb3f64597c723c7c9436619c4b9ef51
Roberto Aguilar <roberto@baremetal.io>
2013-11-16 01:54:24 -0800

    Initial meetme migration

---
+++ meetme/migrations/0001_initial.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Account'
+        db.create_table(u'meetme_account', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('is_superuser', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('username', self.gf('django.db.models.fields.CharField')(unique=True, max_length=30)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
+            ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('date_joined', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('phone', self.gf('phonenumber_field.modelfields.PhoneNumberField')(max_length=128)),
+            ('rating', self.gf('django.db.models.fields.IntegerField')()),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('tagline', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'meetme', ['Account'])
+
+        # Adding M2M table for field groups on 'Account'
+        m2m_table_name = db.shorten_name(u'meetme_account_groups')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('account', models.ForeignKey(orm[u'meetme.account'], null=False)),
+            ('group', models.ForeignKey(orm[u'auth.group'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['account_id', 'group_id'])
+
+        # Adding M2M table for field user_permissions on 'Account'
+        m2m_table_name = db.shorten_name(u'meetme_account_user_permissions')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('account', models.ForeignKey(orm[u'meetme.account'], null=False)),
+            ('permission', models.ForeignKey(orm[u'auth.permission'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['account_id', 'permission_id'])
+
+        # Adding model 'Review'
+        db.create_table(u'meetme_review', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('rating', self.gf('django.db.models.fields.IntegerField')()),
+            ('author', self.gf('django.db.models.fields.related.ForeignKey')(related_name='review_author', to=orm['meetme.Account'])),
+            ('account', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['meetme.Account'])),
+            ('comment', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'meetme', ['Review'])
+
+        # Adding model 'Category'
+        db.create_table(u'meetme_category', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+        ))
+        db.send_create_signal(u'meetme', ['Category'])
+
+        # Adding model 'Activity'
+        db.create_table(u'meetme_activity', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('account', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['meetme.Account'])),
+            ('tagline', self.gf('django.db.models.fields.CharField')(max_length=140)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['meetme.Category'])),
+        ))
+        db.send_create_signal(u'meetme', ['Activity'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Account'
+        db.delete_table(u'meetme_account')
+
+        # Removing M2M table for field groups on 'Account'
+        db.delete_table(db.shorten_name(u'meetme_account_groups'))
+
+        # Removing M2M table for field user_permissions on 'Account'
+        db.delete_table(db.shorten_name(u'meetme_account_user_permissions'))
+
+        # Deleting model 'Review'
+        db.delete_table(u'meetme_review')
+
+        # Deleting model 'Category'
+        db.delete_table(u'meetme_category')
+
+        # Deleting model 'Activity'
+        db.delete_table(u'meetme_activity')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'meetme.account': {
+            'Meta': {'object_name': 'Account'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'phone': ('phonenumber_field.modelfields.PhoneNumberField', [], {'max_length': '128'}),
+            'rating': ('django.db.models.fields.IntegerField', [], {}),
+            'tagline': ('django.db.models.fields.TextField', [], {}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'meetme.activity': {
+            'Meta': {'object_name': 'Activity'},
+            'account': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['meetme.Account']"}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['meetme.Category']"}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tagline': ('django.db.models.fields.CharField', [], {'max_length': '140'})
+        },
+        u'meetme.category': {
+            'Meta': {'object_name': 'Category'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        u'meetme.review': {
+            'Meta': {'object_name': 'Review'},
+            'account': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['meetme.Account']"}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'review_author'", 'to': u"orm['meetme.Account']"}),
+            'comment': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'rating': ('django.db.models.fields.IntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['meetme']
