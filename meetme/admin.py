f3093d35580b03f54370f3c3955861f2546e72ae
Roberto Aguilar <roberto@baremetal.io>
2013-11-16 14:59:21 -0800

    Added models to admin

---
+++ meetme/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from meetme.models import *
+
+
+admin.site.register(Account)
+admin.site.register(Review)
+admin.site.register(Category)
+admin.site.register(Reservation)
