e0b37a3a970be11aff073f3eefc20e9cd9562b81
Roberto Aguilar <roberto@baremetal.io>
2013-11-16 22:58:43 -0800

    Added javascript to make request.

--- meetme/static/js/meetme.js
+++ meetme/static/js/meetme.js
@@ -25,3 +25,35 @@ function spin() {
 }
 
 
+$(function () {
+    var requestButton = $('#request');
+
+    requestButton.click(function () {
+        var buttonWidth = requestButton.width();
+
+        requestButton.width(buttonWidth);
+        requestButton.css({"text-align": "center"});
+        requestButton.html('<div id="spinner"></div>');
+
+        var spinner = $('#spinner');
+        var spinInterval = spin();
+
+        $.ajax({
+            url: '/make_request/',
+            type: 'POST',
+            data: {
+                account_id: localId
+            },
+
+            success: function () {
+                clearInterval(spinInterval);
+
+                spinner.hide();
+                $('#request')
+                    .html("Done!")
+                    .removeClass("btn-info")
+                    .addClass("btn-success");
+            }
+        });
+    });
+});
